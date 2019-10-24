5bdfb0e9474c06439d9e54785ccdd2736811cb98
Hassan Ali <Hassan Ali@DESKTOP-3JVVKJJ>
2019-10-24 22:23:32 +0500

    implement remove, insertion without stack and print

--- StacklessBST.h
+++ StacklessBST.h
@@ -40,9 +40,21 @@ class SortedStacklessBST {
 public:
 	SortedStacklessBST();
 	bool insert(T d);
+	bool insertStackless(T d);
+
+	bool remove(T d);
+
+	void print();
+	void print(T const& high, T const& low);
 private:
 	Node<T>* root;
 	bool IsSuccessor;
+
+	bool remove(T d, Node<T>*& r, Node<T>* prev);
+	Node<T>* detach(T d, Node<T>*& r, Node<T>* prev);
+	void DislocateLeaf(T d, Node<T>*& r, Node<T>* prev);
+
+	void updateNextInOrder(Node<T>*& r, Node<T>* prev);
 
 	//updates successor of parents of new node and finds successor of new node
 	void updateSuccessor(stack<PushNode<T>> traceTree, Node<T>* newNode);
@@ -54,7 +66,7 @@ template<class T>
 SortedStacklessBST<T>::SortedStacklessBST()
 {
 	root = nullptr;
-	IsSuccessor = 1;
+	IsSuccessor = 0;
 }
 
 template<typename T>
@@ -98,6 +110,271 @@ bool SortedStacklessBST<T>::insert(T d)
 	updateSuccessor(traceTree, trav);
 }
 
+template<class T>
+bool SortedStacklessBST<T>::insertStackless(T d)
+{
+	//empty tree
+	if (root == nullptr) {
+		root = new Node<T>(d);
+		return 1;
+	}
+
+	Node<T>* newNode = new Node<T>(d);
+	Node<T>* trav = root;
+	Node<T>* trail = root;
+
+	while (trav != nullptr) {
+
+		//right subtree
+		if (d > trav->data) {
+			if (IsSuccessor == 1) {
+
+				//update trav successor
+				if (trav->nextInOrder == nullptr) {
+					trav->nextInOrder = newNode;
+				}
+				else {
+					if (d < trav->nextInOrder->data) {
+						trav->nextInOrder = newNode;
+					}
+				}
+			}
+			else{
+				//update new node successor
+				newNode->nextInOrder = trav;
+			}
+			trail = trav;
+			trav = trav->right;
+		}
+
+		//left subtree
+		else if (d < trav->data) {
+			if (IsSuccessor == 0) {
+				//update trav successor
+				if (trav->nextInOrder == nullptr) {
+					trav->nextInOrder = newNode;
+				}
+				else {
+					if (d > trav->nextInOrder->data) {
+						trav->nextInOrder = newNode;
+					}
+				}
+			}
+			else  {
+				//update new node successor
+				newNode->nextInOrder = trav;
+			}
+			trail = trav;
+			trav = trav->left;
+		}
+
+		else {
+			delete newNode;
+			return 0;
+		}
+	}
+
+	if (trail->data > d) {
+		trail->left = newNode;
+	}
+	else {
+		trail->right = newNode;
+	}
+	return 1;
+}
+
+template<class T>
+bool SortedStacklessBST<T>::remove(T d)
+{
+	return remove(d, root, nullptr);
+}
+
+template<class T>
+void SortedStacklessBST<T>::print()
+{
+	Node<T>* temp = root;
+
+	if (IsSuccessor == 1) {
+		while (temp != nullptr && temp->left) {
+			temp = temp->left;
+		}
+	}
+	else {
+		while (temp != nullptr && temp->right) {
+			temp = temp->right;
+		}
+	}
+
+	while (temp) {
+		cout << temp->data << " ";
+		temp = temp->nextInOrder;
+	}
+
+}
+
+template<class T>
+void SortedStacklessBST<T>::print(T const& high, T const& low)
+{
+	Node<T>* temp = root;
+
+	if (IsSuccessor == 1) {
+		while (temp != nullptr && temp->left) {
+			temp = temp->left;
+		}
+	}
+	else {
+		while (temp != nullptr && temp->right) {
+			temp = temp->right;
+		}
+	}
+
+	while (temp && temp->data<high) {
+		cout << temp->data << " ";
+		temp = temp->nextInOrder;
+	}
+}
+
+template<class T>
+bool SortedStacklessBST<T>::remove(T d, Node<T>*& r, Node<T>* prev)
+{
+	Node<T>* temp = detach(d, r, prev);
+	if (temp == nullptr)
+		return 0;
+	
+	delete temp;
+	return 1;
+}
+
+template<class T>
+ Node<T>* SortedStacklessBST<T>::detach(T d, Node<T>*& r, Node<T>* prev)
+{
+	 //data not found
+	 if (r == nullptr)
+		 return nullptr;
+	 //right
+	 if (d > r->data) {
+		 if (IsSuccessor == 1)
+			 return detach(d, r->right, r);
+		 else
+			 return detach(d, r->right, prev);
+	 }
+	 //left
+	 else if (d < r->data) {
+		 if (IsSuccessor == 1)
+			 return detach(d, r->left, prev);
+		 else
+			 return detach(d, r->left, r);
+	 }
+	 else
+	 { //node found
+		 //finds the node whose nextInOrder points to r and updates that
+		 updateNextInOrder(r, prev);
+
+		 //delete node
+		 if (r->left == nullptr && r->right == nullptr) {
+			 //case 1
+			 Node<T>* temp = r;
+			 r = nullptr;
+			 return temp;
+		 }
+		 else if (r->left == nullptr) {
+			 //case 2
+			 Node<T>* temp = r;
+			 r = r->right;
+			 return temp;
+		 }
+		 else if (r->right == nullptr) {
+			 //case 2
+			 Node<T>* temp = r;
+			 r = r->left;
+			 return temp;
+		 }
+		 else
+		 {
+			 //case 3
+
+			 //find and detach successor
+			 Node<T>* tempSuc = getChildSuccessor(r);
+			 if (IsSuccessor == 1) {
+				 DislocateLeaf(tempSuc->data, r->right, r);
+			 }
+			 else
+				 DislocateLeaf(tempSuc->data, r->left, r);
+			
+			 //replace successor with r
+			 Node<T>* temp = r;
+			 r = tempSuc;
+			 r->left = temp->left;
+			 r->right = temp->right;
+			
+			 return temp;
+		 }
+
+	 }
+}
+
+ template<class T>
+ void SortedStacklessBST<T>::DislocateLeaf(T d, Node<T>*& r, Node<T>* prev)
+ {
+	 //data not found
+	 if (r == nullptr)
+		 return;
+	 //right
+	 if (d > r->data) {
+		 if (IsSuccessor == 1)
+			 DislocateLeaf(d, r->right, r);
+		 else
+			 DislocateLeaf(d, r->right, prev);
+	 }
+	 //left
+	 else if (d < r->data) {
+		 if (IsSuccessor == 1)
+			 DislocateLeaf(d, r->left, prev);
+		 else
+			 DislocateLeaf(d, r->left, r);
+	 }
+	 else
+	 { //node found
+		 if (r->left == nullptr && r->right == nullptr) {
+			 //case 1
+			 Node<T>* temp = r;
+			 r = nullptr;
+		 }
+	 }
+ }
+
+template<typename T>
+void SortedStacklessBST<T>::updateNextInOrder(Node<T>*& r,Node<T>* prev) {
+	//update nextInOrder of previous node
+	IsSuccessor = !IsSuccessor;
+	Node<T>* prevChild = getChildSuccessor(r);
+	IsSuccessor = !IsSuccessor;
+
+	if (prev == nullptr && prevChild == nullptr)
+		return;
+
+	if (prev != nullptr && prevChild==nullptr) {
+		prev->nextInOrder = r->nextInOrder;
+		return;
+	}
+
+	if (prevChild != nullptr && prev==nullptr) {
+		prevChild->nextInOrder = r->nextInOrder;
+		return;
+	}
+
+	if (IsSuccessor == 1) {
+
+		if (prev->data < prevChild->data)
+			prev = prevChild;
+	}
+	else {
+		if (prev->data > prevChild->data)
+			prev = prevChild;
+	}
+
+	prev->nextInOrder = r->nextInOrder;
+}
 //update the successors of parent nodes
 //find successor of new node
 template<typename T>
@@ -154,7 +431,7 @@ Node<T>* SortedStacklessBST<T>::getChildSuccessor(Node<T>* n)
 	if (IsSuccessor == 1) {
 		//goto right subtree and get the smallest node
 		Node<T>* trav = n->right;
-		while (trav->left != nullptr) {
+		while (trav!=nullptr && trav->left != nullptr) {
 			trav = trav->left;
 		}
 		return trav;
@@ -162,7 +439,7 @@ Node<T>* SortedStacklessBST<T>::getChildSuccessor(Node<T>* n)
 	else {
 		//goto left subtree and get biggest node
 		Node<T>* trav = n->left;
-		while (trav->right != nullptr) {
+		while (trav != nullptr && trav->right != nullptr) {
 			trav = trav->right;
 		}
 		return trav;
