a706e95d8304b080d62e79f15451044c0eb846ff
Hassan Ali <Hassan Ali@DESKTOP-3JVVKJJ>
2019-10-27 12:10:32 +0500

    Remove unused code for insertion using stack

--- StacklessBST.h
+++ StacklessBST.h
@@ -11,12 +11,14 @@ struct Node {
 	Node<T>* right;
 	Node<T>* nextInOrder;
 
+	//default constructor
 	Node() {
 		left = nullptr;
 		right = nullptr;
 		nextInOrder = nullptr;
 	}
 
+	//parametric constructor
 	Node(T d, Node<T>* r = 0, Node<T>* l = 0, Node<T>* next = 0) {
 		data = d;
 		left = l;
@@ -25,23 +27,13 @@ struct Node {
 	}
 };
 
-//stores parent of current node and its direction relative to parent
-template<typename T>
-struct PushNode{
-	enum ChildDirection { left = 0, right = 1 };
-
-	Node<T> * nod;
-	ChildDirection direction;
-	PushNode(Node<T>* n, ChildDirection dir) : nod(n), direction(dir) {};	
-};
-
 template<class T>
 class SortedStacklessBST {
 public:
 	SortedStacklessBST();
 	~SortedStacklessBST();
 	SortedStacklessBST(SortedStacklessBST const&);
-	bool insert(T d);
+	//bool insert(T d);
 	bool insertStackless(T d);
 
 	bool remove(T d);
@@ -72,8 +64,6 @@ private:
 
 	void updateNextInOrder(Node<T>*& r, Node<T>* prev);
 
-	//updates successor of parents of new node and finds successor of new node
-	void updateSuccessor(stack<PushNode<T>> traceTree, Node<T>* newNode);
 	//searches for successor of passed node
 	Node<T>* getChildSuccessor(Node<T>* n);
 };
@@ -95,47 +85,6 @@ template<class T>
 SortedStacklessBST<T>::SortedStacklessBST(SortedStacklessBST const& obj)
 {
 	copy(obj.root);
-}
-
-template<typename T>
-bool SortedStacklessBST<T>::insert(T d)
-{
-	//list empty
-	if (root == nullptr) {
-		root = new Node<T>(d);
-		return 1;
-	}
-
-	//traverse the tree
-	Node<T>* trav = root;
-	//store previous nodes while tracing tree
-	stack<PushNode<T>> traceTree;
-
-	//traverse to find the right location
-	while (trav != nullptr) {
-		if (d > trav->data) {
-			traceTree.push(PushNode<T>(trav, PushNode<T>::ChildDirection::right));
-			trav = trav->right;
-		}
-		else if (d < trav->data) {
-			traceTree.push(PushNode<T>(trav, PushNode<T>::ChildDirection::left));
-			trav = trav->left;
-		}
-		else
-			return 0; //data is already in stack
-	}
-
-	//add new node
-	trav = new Node<T>(d);
-	PushNode<T> temp = traceTree.top();
-	if (temp.direction == temp.left)
-		temp.nod->left =trav;
-	else
-		temp.nod->right =trav;
-
-	//update the successors of parent nodes
-	//find successor of new node
-	updateSuccessor(traceTree, trav);
 }
 
 template<class T>
@@ -545,55 +494,6 @@ void SortedStacklessBST<T>::updateNextInOrder(Node<T>*& r,Node<T>* prev) {
 	}
 
 	prev->nextInOrder = r->nextInOrder;
-}
-//update the successors of parent nodes
-//find successor of new node
-template<typename T>
-void SortedStacklessBST<T>::updateSuccessor(stack<PushNode<T>> traceTree, Node<T>* newNode)
-{
-	//Rules for successor / predecessor
-	//	1. Successor
-	//		a.Possibility 1
-	//			i.Goto right child
-	//			ii.Get min node of right tree
-	//		b.Possibility 2
-	//			i.Go to parent
-	//			ii.Stop when find a rightwards connection
-	//		c.Get max of p1 and p2
-	//	2. Predecessor
-	//		a.Possibility 1
-	//			i.Goto left child
-	//			ii.Get max of left tree
-	//		b.Possibilty 2
-	//			i.Goto parent
-	//			ii.Stop when find left connection
-
-	while (!traceTree.empty()) {
-		PushNode<T> tempParent = traceTree.top();
-		traceTree.pop();
-
-		//find successor of newNode
-		if (IsSuccessor == 1 && tempParent.direction == tempParent.left 
-			&& newNode->nextInOrder == nullptr) {
-
-			newNode->nextInOrder = tempParent.nod;
-		}
-		if (IsSuccessor == 0 && tempParent.direction == tempParent.right
-			&& newNode->nextInOrder == nullptr) {
-
-			newNode->nextInOrder = tempParent.nod;
-		}
-		
-		//update successor of current parent
-		if (IsSuccessor == 1 && tempParent.direction == tempParent.right
-			|| IsSuccessor == 0 && tempParent.direction == tempParent.left) {
-			
-			Node<T>* tempSucc = getChildSuccessor(tempParent.nod);
-			if (tempSucc!=nullptr && tempSucc->data > tempParent.nod->data) {
-				tempParent.nod->nextInOrder = tempSucc;
-			}
-		}
-	}
 }
 
 template<class T>
